e703713933aa87a55965c3f10ae81814fa890ccc
Ge Gao <ge.gao@momenta.ai>
2023-07-11 14:47:23 +0800

    🌈 style(backend): Snake

--- code/fastapi_snake_app/fastapi_snake_app/game.py
+++ code/fastapi_snake_app/fastapi_snake_app/game.py
@@ -13,8 +13,17 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 
-class Point:  # 使用 Point 类来接受 row 横坐标, col 纵坐标
+class Point:
+    """
+    坐标类
+    """
+
     def __init__(self, row: int = 0, col: int = 0):
+        """
+        :param row: 横坐标
+        :param col: 纵坐标
+        """
+
         self.row = row
         self.col = col
 
@@ -25,20 +34,36 @@ class Point:  # 使用 Point 类来接受 row 横坐标, col 纵坐标
 Direction: TypeAlias = Literal['left', 'right', 'up', 'down']
 
 
-class Snake:  # 定义贪吃蛇类
-    def __init__(self, *, length: int, color: str, row: int, col: int, height: int = 400, width: int = 400):
+class Snake:
+    """
+    贪吃蛇类
+    """
+
+    def __init__(self, *, length: int, color: str, row: int, col: int,
+                 height: int = 400, width: int = 400) -> None:
+        """
+        :param length: 蛇身长度
+        :param color: 蛇身颜色F
+        :param row: 蛇头横坐标
+        :param col: 蛇头纵坐标
+        :param height: 场地高度, 默认 400
+        :param width: 场地宽度, 默认 400
+        """
+
         self.length = length  # 设置蛇身长度
         self.color = color
         self.playground_height = height  # 设置场地高度
         self.playground_width = width  # 设置场地宽度
         self.head = Point(row, col)  # 设置初始蛇头坐标
         self.snake_body: list[Point] = []  # 一个数组 用于保存蛇的身体坐标
+
         for i in range(length):  # 根据长度初始化蛇身体
             body_part = Point(row, col+i)  # 初始蛇身横向排列
             self.snake_body.append(body_part)
+
         self.direction: Direction = 'left'  # 默认初始移动方向为向左
 
-    def move(self, direction_next: Direction = 'left'):  # 默认未来移动方向为向左
+    def move(self, direction_next: Direction = 'left') -> None:  # 默认未来移动方向为向左
         head = self.head.copy()
         # 注意移动方向不能与当前前进方向相反，并且在获取未来移动方向后更新head坐标
         if direction_next == 'up' and self.direction in ['left', 'right']:
@@ -57,18 +82,20 @@ class Snake:  # 定义贪吃蛇类
             self.direction == 'right'
             head.col += 1
             self.head.col += 1
+
         # 在更新完毕蛇头位置之后，需要更新蛇体位置,把蛇头插入最新位置，即可更新所有点的位置，然后把最后一个点pop即可
         self.snake_body.insert(0, head)
         self.snake_body.pop()
 
-    def hit_wall_check(self):  # 检测是否撞到墙
+    def hit_wall_check(self) -> bool:  # 检测是否撞到墙
         dead = False
         head = self.head.copy()
-        if head.col < 0 or head.row < 0 or head.row > self.playground_height or head.col > self.playground_width:
+        if (head.col < 0 or head.row < 0 or
+                head.row > self.playground_height or head.col > self.playground_width):
             dead = True
         return dead
 
-    def hit_body_check(self):  # 检测是否撞到自己
+    def hit_body_check(self) -> bool:  # 检测是否撞到自己
         dead = False
         head = self.head.copy()
         for snake_body_points in self.snake_body[1:]:  # 注意排除第0个元素 即蛇头自身
