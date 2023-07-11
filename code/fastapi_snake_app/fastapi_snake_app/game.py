eea6a795162c409c531cababec9931a8adad21a8
xzy-luis <13775369545@qq.com>
2023-07-11 15:05:01 +0800

    feat(后端): 增加贪吃蛇类

--- code/fastapi_snake_app/fastapi_snake_app/game.py
+++ code/fastapi_snake_app/fastapi_snake_app/game.py
@@ -4,7 +4,6 @@ from fastapi import WebSocket
 
 from fastapi_snake_app.main import app
 
-
 games = {}
 # 颜色
 BLACK = (0, 0, 0)
@@ -56,9 +55,9 @@ class Snake:
         self.playground_width = width  # 设置场地宽度
         self.head = Point(row, col)  # 设置初始蛇头坐标
         self.snake_body: list[Point] = []  # 一个数组 用于保存蛇的身体坐标
-
+        self.dead = False  # 表明是否死亡
         for i in range(length):  # 根据长度初始化蛇身体
-            body_part = Point(row, col+i)  # 初始蛇身横向排列
+            body_part = Point(row, col + i)  # 初始蛇身横向排列
             self.snake_body.append(body_part)
 
         self.direction: Direction = 'left'  # 默认初始移动方向为向左
@@ -87,7 +86,7 @@ class Snake:
         self.snake_body.insert(0, head)
         self.snake_body.pop()
 
-    def hit_wall_check(self) -> bool:  # 检测是否撞到墙
+    def is_hit_wall(self) -> bool:  # 检测是否撞到墙
         dead = False
         head = self.head.copy()
         if (head.col < 0 or head.row < 0 or
@@ -95,7 +94,7 @@ class Snake:
             dead = True
         return dead
 
-    def hit_body_check(self) -> bool:  # 检测是否撞到自己
+    def is_hit_body(self) -> bool:  # 检测是否撞到自己
         dead = False
         head = self.head.copy()
         for snake_body_points in self.snake_body[1:]:  # 注意排除第0个元素 即蛇头自身
@@ -103,6 +102,51 @@ class Snake:
                 dead = True
                 break
         return dead
+
+
+def hit_snake_check(snake1: Snake, snake2: Snake):
+    head1 = snake1.head.copy()
+    head2 = snake2.head.copy()
+    body1 = snake1.snake_body.copy()
+    body2 = snake2.snake_body.copy()
+    for snake_body1_points in body1:
+        if snake_body1_points.row == head2.row and snake_body1_points.col == head2.col:
+            snake2.dead = True
+            break
+    for snake_body2_points in body2:
+        if snake_body2_points.row == head1.row and snake_body2_points.col == head1.col:
+            snake1.dead = True
+            break
+
+
+def game_judgement(snake1: Snake, snake2: Snake):
+    """
+    4个int返回值，
+    0：没有胜负发生
+    1：snake1胜利
+    2：snake2胜利
+    3：平局
+    """
+    if snake1.is_hit_wall():
+        snake1.dead = True
+    if snake1.is_hit_body():
+        snake1.dead = True
+    if snake2.is_hit_wall():
+        snake2.dead = True
+    if snake2.is_hit_body():
+        snake2.dead = True
+    hit_snake_check(snake1, snake2)
+    if snake1.dead and snake2.dead:
+        print("Tie!")
+        return 3
+    elif snake2.dead is True and snake1.dead is False:
+        print("snake1 wins!")
+        return 1
+    elif snake1.dead is True and snake2.dead is False:
+        print("snake2 wins")
+        return 2
+    else:
+        return 0
 
 
 @app.get('/echo')
@@ -121,4 +165,5 @@ async def websocket_endpoint(websocket: WebSocket) -> NoReturn:
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
+
         await websocket.send_text(f'Message text was: {data}')
