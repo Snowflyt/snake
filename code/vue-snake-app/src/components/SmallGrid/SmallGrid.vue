d54c50a2a291e8623bc6c907bb909bf37b6d39ec
tony <280880907@163.com>
2023-07-11 10:03:48 +0800

    components中增加关卡格式样件

---
+++ code/vue-snake-app/src/components/SmallGrid/SmallGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="PassPic">
+    <div
+      class="flex h-[70%] content-center items-center justify-center border-2 border-black">
+      <div>
+        {{ number }}
+      </div>
+      <div>
+        <el-rate v-model="value1" :color="color" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      size: 'large',
+      number: 8,
+      value1: null,
+      color: null,
+    };
+  },
+};
+</script>
