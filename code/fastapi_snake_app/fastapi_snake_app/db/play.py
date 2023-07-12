e08389953c1860615e74dd60c50cf33f6b67065f
Ge Gao <ge.gao@momenta.ai>
2023-07-12 10:32:45 +0800

    🐞 fix(backend): 修复 Model 定义问题

--- code/fastapi_snake_app/fastapi_snake_app/db/play.py
+++ code/fastapi_snake_app/fastapi_snake_app/db/play.py
@@ -1,8 +1,8 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel  # type: ignore
 
 
 class Play(SQLModel, table=True):
     user_id: str = Field(default=id, nullable=False, primary_key=True)
     stage_id: str = Field(default=False, nullable=False, primary_key=True)
     is_locked: bool = Field(nullable=False)
-    stars_already_get: int = Field(max_items=3, min_items=0, nullable=False)
+    stars_already_get: int = Field(nullable=False)
--- code/fastapi_snake_app/fastapi_snake_app/db/user.py
+++ code/fastapi_snake_app/fastapi_snake_app/db/user.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel  # type: ignore
 
 
 class User(SQLModel, table=True):
@@ -6,10 +6,10 @@ class User(SQLModel, table=True):
     username: str = Field(default=id, nullable=False, unique=True)
     password: str = Field(nullable=False, max_length=20, min_length=8)
     phone_number: str = Field(nullable=False, min_length=11, max_length=11)
-    level: int = Field(min_items=1, nullable=False)
+    level: int = Field(nullable=False)
     experience: int = Field(nullable=False)
-    common_money: int = Field(nullable=False, min_items=0)
-    special_money: int = Field(nullable=False, min_items=0)
+    common_money: int = Field(nullable=False)
+    special_money: int = Field(nullable=False)
     total_stars: int = Field(default=0, nullable=False)
     rank: str = '最强青铜IV'
-    score: int = Field(default=0, min_items=0, max_items=100)
+    score: int = Field(default=0)
