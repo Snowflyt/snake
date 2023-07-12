7d61914e88ced31708af89e4c50b17d450d5a5f1
Snowflyt <gaoge011022@163.com>
2023-07-12 11:23:24 +0800

    🐳 chore(frontend): 更新 Proxy 到测试服务器

--- code/vue-snake-app/vite.config.ts
+++ code/vue-snake-app/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://101.132.165.23:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
