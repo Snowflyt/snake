e672ef8f213a6a0a4f30ac2d7d3887de30ad8644
ArcadeFire1024 <2198151409@qq.com>
2023-07-12 11:04:01 +0800

    格式化了一下

--- code/fastapi_snake_app/fastapi_snake_app/db/stage.py
+++ code/fastapi_snake_app/fastapi_snake_app/db/stage.py
@@ -4,5 +4,5 @@ from sqlmodel import Field, SQLModel
 class Stage(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     sname: str = Field(default=id, nullable=False, unique=True)
-    isLocked: bool=Field(default=False,nullable=False)
-    unlockPreconditionStars: int=Field(nullable=False)
+    isLocked: bool = Field(default=False, nullable=False)
+    unlockPreconditionStars: int = Field(nullable=False)
