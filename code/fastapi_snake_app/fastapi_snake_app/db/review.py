5d6e1993277917f571cdebb1e910cbc1ac84e4ab
ArcadeFire1024 <2198151409@qq.com>
2023-07-13 21:51:11 +0800

    完成了review类的修改

--- code/fastapi_snake_app/fastapi_snake_app/db/review.py
+++ code/fastapi_snake_app/fastapi_snake_app/db/review.py
@@ -4,8 +4,9 @@ from sqlmodel import Field, SQLModel  # type: ignore
 
 
 class Review(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    username: str|None = Field(default=str(id) if id else None)
+    id: int = Field(primary_key=True,nullable=False)
+    user_id:int=Field(default=None,nullable=False)
+    username: str|None = Field(default=str(user_id) if id else None)
     content: str=Field(nullable=False)
     submit_time: datetime=Field(default=datetime.today())
 
