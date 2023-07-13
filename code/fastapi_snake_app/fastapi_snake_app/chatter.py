6477fa1c783cb351214e9fb3c3c8fa881ca8fe29
ArcadeFire1024 <2198151409@qq.com>
2023-07-13 18:22:03 +0800

    对聊天室进行了更改

--- code/fastapi_snake_app/fastapi_snake_app/chatter.py
+++ code/fastapi_snake_app/fastapi_snake_app/chatter.py
@@ -28,10 +28,12 @@ html = """
             var ws = new WebSocket(`ws://101.132.165.23:8000/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
+                var messageOut = document.createElement('li');
                 var message = document.createElement('li')
                 var content = document.createTextNode(event.data)
                 message.appendChild(content)
-                messages.appendChild(message)
+                messageOut.appendChild(message);
+                messages.appendChild(messageOut);
             };
             function sendMessage(event) {
                 var input = document.getElementById("messageText")
@@ -86,9 +88,7 @@ async def websocket_endpoint(client_id: str, websocket: WebSocket):
             data = await websocket.receive_text()
             # 4、广播某个客户端发送的消息
             await manager.broadcast(f"{client_id} 发送消息：{data}")
-            # 5、服务端回复客户端
-            await manager.send_personal_message(f"服务端回复{client_id}：你发送的信息是：{data}", websocket)
     except WebSocketDisconnect:
-        # 6、若有客户端断开连接，广播某个客户端离开了
+        # 5、若有客户端断开连接，广播某个客户端离开了
         manager.disconnect(websocket)
         await manager.broadcast(f"{client_id} 离开了聊天室")
