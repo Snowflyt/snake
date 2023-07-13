dea267092b9c89f862b8cd5a4a2b5a337abc18c0
ArcadeFire1024 <2198151409@qq.com>
2023-07-13 15:31:18 +0800

    完成了头像上传的开发

--- code/fastapi_snake_app/fastapi_snake_app/db/user.py
+++ code/fastapi_snake_app/fastapi_snake_app/db/user.py
@@ -14,5 +14,6 @@ class User(SQLModel, table=True):
     rank: str = '最强青铜IV'
     score: int = Field(default=0)
     language_excellent:str=Field(default="全能")
+    profile_URL:str=Field(nullable=False,default="http://127.0.0.1:8000/profiles/default_profile.jpg")
 
 
